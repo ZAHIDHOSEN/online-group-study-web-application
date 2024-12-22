2e37d8603b386fa3fc1bc951b6a365a083f602a4
ZAHIDHOSEN <zahidhosen203@gmail.com>
2024-12-22 14:53:23 +0600

    basic router setup

---
+++ src/router/router.jsx
@@ -0,0 +1,23 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../Pages/Home/Home";
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      errorElement: <h2>Route not Found</h2>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+
+        }
+      ]
+    },
+  ]);
+
+  export default router;
